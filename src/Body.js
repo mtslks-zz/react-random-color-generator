21584ab823314fb3a3b8dcc742979f927353a4a9
Mathias Lukas <mathias.lukas@me.com>
2021-10-07 09:56:25 +0200

    Update library, bug fixing

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,9 @@
 import './App.css';
-// import styled from '@emotion/styled'; // CSS-in-JS API
+import color from 'randomcolor';
 import { useState } from 'react';
 
-export default Body;
-
 // Library import to randomize colors
-const color = require('randomcolor');
+// const color = require('randomcolor');
 
 // Setting component Body that will use the library to set a random color as background color
 function Body() {
@@ -35,36 +33,38 @@ function Body() {
 
       <div>
         <b>Care for some hue?</b>
-        <p />
-        <select
-          className="hueDropdown"
-          value={hue}
-          onChange={(event) => setHue(event.target.value)}
-        >
-          <option value="random">surprise me!</option>
-          <option value="red">red</option>
-          <option value="green">green</option>
-          <option value="blue">blue</option>
-          <option value="yellow">yellow</option>
-          <option value="orange">orange</option>
-          <option value="purple">purple</option>
-          <option value="pink">pink</option>
-        </select>
+        <p>
+          <select
+            className="hueDropdown"
+            value={hue}
+            onChange={(event) => setHue(event.target.value)}
+          >
+            <option value="random">surprise me!</option>
+            <option value="red">red</option>
+            <option value="green">green</option>
+            <option value="blue">blue</option>
+            <option value="yellow">yellow</option>
+            <option value="orange">orange</option>
+            <option value="purple">purple</option>
+            <option value="pink">pink</option>
+          </select>
+        </p>
       </div>
 
       <div>
         <b>Pick your luminosity!</b>
-        <p />
-        <select
-          className="luminosityDropdown"
-          value={luminosity}
-          onChange={(event) => setLuminosity(event.target.value)}
-        >
-          <option value="random">surprise me!</option>
-          <option value="dark">dark</option>
-          <option value="bright">bright</option>
-          <option value="light">light</option>
-        </select>
+        <p>
+          <select
+            className="luminosityDropdown"
+            value={luminosity}
+            onChange={(event) => setLuminosity(event.target.value)}
+          >
+            <option value="random">surprise me!</option>
+            <option value="dark">dark</option>
+            <option value="bright">bright</option>
+            <option value="light">light</option>
+          </select>
+        </p>
       </div>
 
       <div>
@@ -72,9 +72,10 @@ function Body() {
           Randomly generated banana factor <p />
           {randomColor}
         </b>
-        <p />
-        #bananarama!!!
+        <p>#bananarama!!!</p>
       </div>
     </body>
   );
 }
+
+export default Body;
